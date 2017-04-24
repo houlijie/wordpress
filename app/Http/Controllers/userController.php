e4409d94fd08b93ad3c39b694c2e0ddaec00ec67
houlijie <houlijie@shopex.cn>
2017-04-24 17:26:40 +0800

    [edit]userController

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\User;
 
 class userController extends Controller
 {
+    public function update(Request $request){
+        $name = $request->input('id', '2');
+        $parentName = $request->input('user.parent.name');
+
+    }
     /**
      *获取用户列表
      *
@@ -22,7 +28,22 @@ class userController extends Controller
      *
      */
     public function userInfo($userId) {
-        
+
         return User::findorFail($userId);
+    }
+
+    public function upload(Request $request) {
+
+        //验证文件在请求中是否存在
+        if($request->hasFile('photo')) {
+            //
+        }
+
+        //验证文件在上传过程中是否出错
+        if($request->file('photo')->isValid()) {
+            $destinationPath = app_env('/');
+            $request->file('photo')->move($destinationPath, $filname);
+        }
+
     }
 }
